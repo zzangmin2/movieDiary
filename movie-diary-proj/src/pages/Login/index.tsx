077e2e327f2893c08bf63c2ddd17c325b3b9cd6b
sharon7224@gmail.com <103804129+zzangmin2@users.noreply.github.com>
2024-01-18 23:20:11 +0900

    20240118
    json-server 사용하기

--- movie-diary-proj/src/pages/Login/index.tsx
+++ movie-diary-proj/src/pages/Login/index.tsx
@@ -6,10 +6,13 @@ import { parse } from "path";
 import { RootState } from "../../redux/rootReducer";
 import { useSelector } from "react-redux";
 import { login } from "../../redux/auth/authSlice";
+import axios from "axios";
 
 const Login: React.FC = () => {
   const dispatch = useDispatch();
-  const [user, setUser] = useState({ email: "", password: "", userName: "" });
+  // const [user, setUser] = useState({ email: "", password: "", userName: "" });
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
   const navigate = useNavigate();
   const loggedInUserData = useSelector((state: RootState) => state.auth);
 
@@ -20,25 +23,60 @@ const Login: React.FC = () => {
   }, [loggedInUserData, navigate]);
 
   const handleLogin = () => {
-    const storedUserData = localStorage.getItem("user");
-    const userArray = storedUserData ? JSON.parse(storedUserData) : {};
-
-    for (let i = 0; i < userArray.length; i++) {
-      const userData = userArray[i];
-      if (
-        userData.email === user.email &&
-        userData.password === user.password
-      ) {
-        alert("로그인 성공!!");
-        localStorage.setItem("authUser", JSON.stringify(userData));
-        dispatch(login(userData));
-        navigate("/");
-        return;
-      }
-    }
-
-    alert("이메일 또는 비밀번호를 확인해주세요!");
+    axios
+      .get("http://localhost:4000/user")
+      .then((response) => {
+        const user = response.data.find(
+          (userData: any) =>
+            userData.email === email && userData.password === password
+        );
+        if (user) {
+          localStorage.setItem("authUser", JSON.stringify(user));
+          dispatch(login(user));
+          console.log("axios 로그인 성공");
+        } else {
+          alert("이메일 또는 비밀번호를 확인해주세요!");
+        }
+      })
+      .catch((err) => {
+        console.error("로그인 실패", err);
+      });
   };
+  // const handleLogin = () => {
+  //   axios
+  //     .post("http://localhost:4000/user", {
+  //       email: user.email,
+  //       password: user.password,
+  //     })
+  //     .then((res) => {
+  //       console.log(res.data);
+  //       // localStorage.clear();
+  //       // localStorage.setItem("userEmail", res.data.email);
+  //       // window.location.replace("http://localhost:3000/");
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+
+  //   const storedUserData = localStorage.getItem("user");
+  //   const userArray = storedUserData ? JSON.parse(storedUserData) : {};
+
+  //   for (let i = 0; i < userArray.length; i++) {
+  //     const userData = userArray[i];
+  //     if (
+  //       userData.email === user.email &&
+  //       userData.password === user.password
+  //     ) {
+  //       alert("로그인 성공!!");
+  //       localStorage.setItem("authUser", JSON.stringify(userData));
+  //       dispatch(login(userData));
+  //       navigate("/");
+  //       return;
+  //     }
+  //   }
+
+  //   alert("이메일 또는 비밀번호를 확인해주세요!");
+  // };
 
   return (
     <S.SignUp>
@@ -48,14 +86,16 @@ const Login: React.FC = () => {
           <div>이메일</div>
           <input
             type="email"
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            // onChange={(e) => setUser({ ...user, email: e.target.value })}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </S.InputWrapper>
         <S.InputWrapper>
           <div>비밀번호</div>
           <input
             type="password"
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            // onChange={(e) => setUser({ ...user, password: e.target.value })}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </S.InputWrapper>
         <S.Button onClick={handleLogin}>로그인</S.Button>
--- movie-diary-proj/src/pages/SignUp/index.tsx
+++ movie-diary-proj/src/pages/SignUp/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./style";
 import { parse } from "path";
+import axios from "axios";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -35,63 +36,85 @@ const SignUp = () => {
     [userName]
   );
 
-  const handleSignUp = useCallback(() => {
-    if (!email) {
-      alert("이메일을 입력해주세요");
+  // const handleSignUp = useCallback(() => {
+  //   if (!email) {
+  //     alert("이메일을 입력해주세요");
+  //     return;
+  //   }
+
+  //   if (!password) {
+  //     alert("비밀번호를 입력해주세요");
+  //     return;
+  //   }
+
+  //   if (!confirmPassword) {
+  //     alert("비밀번호 확인을 입력해주세요");
+  //     return;
+  //   }
+
+  //   if (!userName) {
+  //     alert("닉네임을 입력해주세요");
+  //     return;
+  //   }
+  //   if (password !== confirmPassword) {
+  //     alert("비밀번호가 일치하지 않습니다.");
+  //     return;
+  //   }
+
+  //   const storedUserData = localStorage.getItem("user");
+  //   const userArray = storedUserData ? JSON.parse(storedUserData) : [];
+  //   console.log(userArray);
+
+  //   for (let i = 0; i < userArray.length; i++) {
+  //     const userData = userArray[i];
+
+  //     if (userData) {
+  //       if (userData.email === email) {
+  //         alert("이미 사용 중인 이메일입니다.");
+  //         return;
+  //       }
+  //       if (userData.userName === userName) {
+  //         alert("이미 사용 중인 닉네임입니다.");
+  //         return;
+  //       }
+  //     }
+  //   }
+
+  //   const newUserData = {
+  //     email: email,
+  //     password: password,
+  //     userName: userName,
+  //   };
+
+  //   userArray.push(newUserData);
+
+  //   localStorage.setItem("user", JSON.stringify(userArray));
+
+  //   alert("회원가입 완료!");
+  //   navigate("/login");
+  // }, [email, password, confirmPassword, userName, navigate]);
+
+  const handleSignUp = () => {
+    if (!email || !password || !confirmPassword || !userName) {
+      alert("모든 필드를 입력해주세요.");
       return;
     }
 
-    if (!password) {
-      alert("비밀번호를 입력해주세요");
-      return;
-    }
-
-    if (!confirmPassword) {
-      alert("비밀번호 확인을 입력해주세요");
-      return;
-    }
-
-    if (!userName) {
-      alert("닉네임을 입력해주세요");
-      return;
-    }
     if (password !== confirmPassword) {
       alert("비밀번호가 일치하지 않습니다.");
       return;
     }
 
-    const storedUserData = localStorage.getItem("user");
-    const userArray = storedUserData ? JSON.parse(storedUserData) : [];
-    console.log(userArray);
-
-    for (let i = 0; i < userArray.length; i++) {
-      const userData = userArray[i];
-
-      if (userData) {
-        if (userData.email === email) {
-          alert("이미 사용 중인 이메일입니다.");
-          return;
-        }
-        if (userData.userName === userName) {
-          alert("이미 사용 중인 닉네임입니다.");
-          return;
-        }
-      }
-    }
-
-    const newUserData = {
-      email: email,
-      password: password,
-      userName: userName,
-    };
-
-    userArray.push(newUserData);
-
-    localStorage.setItem("user", JSON.stringify(userArray));
-
-    alert("회원가입 완료!");
-    navigate("/login");
-  }, [email, password, confirmPassword, userName, navigate]);
+    axios
+      .post("http://localhost:4000/user", { email, password, userName })
+      .then((response) => {
+        alert("회원가입 완료!");
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.error("회원가입 실패");
+      });
+  };
 
   return (
     <S.SignUp>
